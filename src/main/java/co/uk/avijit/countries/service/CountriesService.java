58baa1fc6d074077abfa181aa74409cc6f105d49
avijit_maiti <avijit.haldia@gmail.com>
2025-07-29 01:43:47 +0100

    structuring Service class

--- src/main/java/co/uk/avijit/countries/service/CountriesService.java
+++ src/main/java/co/uk/avijit/countries/service/CountriesService.java
@@ -1,4 +1,20 @@
 package co.uk.avijit.countries.service;
 
+import co.uk.avijit.countries.models.Countries;
+import org.apache.hc.client5.http.classic.HttpClient;
+
+import java.util.Collections;
+import java.util.List;
+
 public class CountriesService {
+
+    private HttpClient httpClient;
+
+    public CountriesService(HttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
+    public List<Countries> getCountries() {
+        return Collections.emptyList();
+    }
 }
